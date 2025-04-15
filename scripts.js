abaf10e5a55c63a336ea935aada6c881bf38bf53
andreskull2 <andreskull1@gmail.com>
2025-04-15 12:02:32 -0300

    Atualização de câmbio

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 // Cotação de moedas do dia.
-const USD = 5.85
-const EUR = 6.63
-const GBP = 7.74
+const USD = 5.88
+const EUR = 6.66
+const GBP = 7.79
 
 // Obtendo os elementos do formulário.
 const form = document.querySelector("form")
