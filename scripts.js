689af071a69ae04bb739554757270f044c7bb354
andreskull2 <andreskull1@gmail.com>
2025-10-07 10:28:01 -0300

    Update curency values

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 // Cotação de moedas do dia.
-const USD = 5.335;    
-const EUR = 6.265; 
-const GBP = 7.190;
+const USD = 5.33;    
+const EUR = 6.22; 
+const GBP = 7.16;
 
 // Obtendo os elementos do formulário.
 const form = document.querySelector("form"); // formulário 
