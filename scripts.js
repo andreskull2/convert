46c265b8e275118cb6880456d7acdfae567e1ab6
andreskull2 <andreskull1@gmail.com>
2026-02-07 15:49:50 -0300

    Upadate currency values

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 // Cotação de moedas do dia.
-const USD = 5.24;    
-const EUR = 6.17; 
-const GBP = 7.12;
+const USD = 5.22;    
+const EUR = 6.16; 
+const GBP = 7.10;
 
 // Obtendo os elementos do formulário.
 const form = document.querySelector("form"); // formulário 
