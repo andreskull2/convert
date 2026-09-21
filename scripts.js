dc16d875502613f79e5a3dfc4708277c3bc8d485
andreskull2 <andreskull1@gmail.com>
2026-09-20 21:38:51 -0300

    Update currency values

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 // Cotação de moedas do dia.
-const USD = 5.141;    
-const EUR = 5.904; 
-const GBP = 6.886;
+const USD = 5.14;    
+const EUR = 5.90; 
+const GBP = 6.88;
 
 // Obtendo os elementos do formulário.
 const form = document.querySelector("form"); // formulário 
